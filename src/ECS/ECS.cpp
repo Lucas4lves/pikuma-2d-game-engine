3818aaa235810561ee52c572302dd38c42be1053
lucas4lves <zombnfl@gmail.com>
2024-02-25 23:32:38 -0300

    Entity and System headers

--- src/ECS/ECS.cpp
+++ src/ECS/ECS.cpp
@@ -1,3 +1,8 @@
 #include "./ECS.h"
 
 //TODO: Implement all methods from ECS.h classes 
+int Entity::GetId(){
+    return id;
+}
+
+
--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -1,15 +1,49 @@
 #ifndef ECS_H
 #define ECS_H
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENTS = 32;
+/////////////////////////////////////////////////////////////
+// Signature
+/////////////////////////////////////////////////////////////
+// 
+/////////////////////////////////////////////////////////////
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
 class Components{
     //TODO
 };
 
 class Entity{
     //TODO
+    private:
+        int id;
+    public:
+        Entity();
+        Entity(int _id) : _id(id){}; //Initializing list
+        int GetId() const; //Methods that does not modify the state of an entity
 };
+
+//////////////////////////////
+//////// SYSTEMS /////////////
+/////Processes entities that contain specific signatures////////
+//////////////////////////////
 
 class System{
     //TODO
+   private:
+        Signature _componentSignature; 
+        std::vector<Entity> _entities;
+    public:
+        System() = default;
+        ~System() = default;
+
+        void AddEntityToSystem(Entity e);
+        void RemoveEntityFromSystem(Entity e);
+        std::vector<Entity> GetSystemEntities() const;
+        Signature& GetComponentSignature() const;
+
 };
 
 class Registry{
